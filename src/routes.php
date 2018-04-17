82919c1956aedbb0e6a53e8fe17ba910fc52ded0
Martin Boer <martin@myparcel.com>
2018-04-17 12:30:18 +0200

    :ok_hand: moved the authenticated api instance to a dependency

--- src/routes.php
+++ src/routes.php
@@ -8,32 +8,18 @@ $app->get('/', function (Request $request, Response $response) {
     return $this->renderer->render($response, 'index.phtml');
 });
 
-// The route to retrieve new Pickup Dropoff Locations.
+// The route to retrieve new pick-up and drop-off locations.
 $app->get('/locations/{countryCode}/{postalCode}', function (Request $request, Response $response, array $arguments) {
-    // The postal code and country code are passed as path variables and retrieved from the request object.
+    // The postal code and country code are passed as path variables and retrieved from the arguments.
     $countryCode = $arguments['countryCode'];
     $postalCode = $arguments['postalCode'];
 
-    // Load a MyParcelComApi instance.
-    $api = new \MyParcelCom\ApiSdk\MyParcelComApi(
-        $this->get('settings')['myparcelcom_api_url']
-    );
-
-    // Pass your authentication credentials.
-    $authenticator = new \MyParcelCom\ApiSdk\Authentication\ClientCredentials(
-        $this->get('settings')['myparcelcom_credentials']['client_id'],
-        $this->get('settings')['myparcelcom_credentials']['client_secret'],
-        $this->get('settings')['myparcelcom_auth_url']
-    );
-    $api->authenticate($authenticator);
-
-    // Get the Pickup Dropoff Locations through the sdk.
-    $locations = $api->getPickUpDropOffLocations($countryCode, $postalCode);
+    // Get the locations through the authenticated API instance.
+    $locations = $this->api->getPickUpDropOffLocations($countryCode, $postalCode);
 
     // Merge all the locations to a single array.
     $allLocations = array_reduce($locations, function (array $combinedLocations, $carrierLocations) {
-        // If the locations for a specific carriers is `null`, it means there
-        // was an error retrieving them.
+        // If the locations for a specific carriers is `null`, it means there was an error retrieving them.
         if ($carrierLocations === null) {
             return $combinedLocations;
         }
@@ -48,23 +34,13 @@ $app->get('/locations/{countryCode}/{postalCode}', function (Request $request, R
     ]);
 });
 
-// The route to retrieve the Carriers.
+// The route to retrieve available carriers.
 $app->get('/carriers', function (Request $request, Response $response, array $arguments) {
-    // Load a MyParcelComApi instance.
-    $api = new \MyParcelCom\ApiSdk\MyParcelComApi(
-        $this->get('settings')['myparcelcom_api_url']
-    );
+    // Get the carriers through the authenticated API instance.
+    $carriers = $this->api->getCarriers()->get();
 
-    // Pass your authentication credentials.
-    $authenticator = new \MyParcelCom\ApiSdk\Authentication\ClientCredentials(
-        $this->get('settings')['myparcelcom_credentials']['client_id'],
-        $this->get('settings')['myparcelcom_credentials']['client_secret'],
-        $this->get('settings')['myparcelcom_auth_url']
-    );
-    $api->authenticate($authenticator);
-
-    // Pass through retrieved locations as a json response.
+    // Pass through retrieved carriers as a json response.
     return $response->withJson([
-        'data' => $api->getCarriers()->get(),
+        'data' => $carriers,
     ]);
 });
